4f39ad9151aaf5313b319d91b479be2c5dfcec96
Adam Percival <Percival.Adam@student.greenriver.edu>
2022-02-28 01:01:25 -0500

    added index page and created JSON array in script file

---
+++ Chapter-4/script/json-search.js
@@ -0,0 +1,46 @@
+window.onload = function() {
+    const people = [
+        {
+            "Name": "Jane Doe",
+            "Sex": "f",
+            "Born": "1876",
+            "Died": "1956",
+            "Father": "Petreous de Milliano",
+            "Mother": "Sophia van Damme"
+        },
+        {
+            "Name": "Jane Ramirez",
+            "Sex": "f",
+            "Born": "1102",
+            "Died": "--",
+            "Father": "Thermistocles Merovech",
+            "Mother": "Amara Floros"
+        },
+        {
+            "Name": "James Cameron",
+            "Sex": "m",
+            "Born": "1982",
+            "Died": "2222",
+            "Father": "Herman Straight",
+            "Mother": "Alena Vegarov"
+        },
+        {
+            "Name": "Colby Jeane",
+            "Sex": "f",
+            "Born": "1802",
+            "Died": "1891",
+            "Father": "Humphrey Charles",
+            "Mother": "Tanya Teneson"
+        },
+        {
+            "Name": "Simon Whay",
+            "Sex": "m",
+            "Born": "2000",
+            "Died": "--",
+            "Father": "Philip Martin",
+            "Mother": "Danielle Peace"
+        }
+    ];
+
+
+}
